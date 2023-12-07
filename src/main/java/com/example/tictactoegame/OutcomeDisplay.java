707589ff17e3f771e501d31c5ebc6052c75963b4
Girish <45308550+gmadnani@users.noreply.github.com>
2023-12-06 19:37:33 -0500

    fuctions for OutcomeDisplay Class

--- src/main/java/com/example/tictactoegame/OutcomeDisplay.java
+++ src/main/java/com/example/tictactoegame/OutcomeDisplay.java
@@ -5,6 +5,20 @@ import javafx.stage.Stage;
 
 public class OutcomeDisplay {
     public static void displayOutcome(String winner, Stage gameStage) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Game Over");
+        alert.setHeaderText(null);
 
+        if (winner.equals("X")) {
+            alert.setContentText("Player X wins!");
+        } else if (winner.equals("O")) {
+            alert.setContentText("Player O wins!");
+        } else {
+            alert.setContentText("It's a draw!");
+        }
+
+        alert.showAndWait();
+
+        resetGame();
     }
 }
